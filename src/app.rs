e063ad6217a7b5f30374f90317b7955f238f5193
rushmi0 <gcalku@gmail.com>
2024-09-07 14:16:27 +0700

    update

--- src/app.rs
+++ src/app.rs
@@ -5,11 +5,13 @@ use crate::pages::HomePage;
 
 const _TAILWIND_URL: &str = manganis::mg!(file("public/tailwind.css"));
 const STYLE: &str = manganis::mg!(file("src/styles/main.css"));
+const ICON: &str = manganis::mg!(file("src/assets/favicon.ico"));
 
 
 #[component]
 pub fn App() -> Element {
     rsx! {
+        link { rel: "icon", href: ICON }
         link { rel: "stylesheet", href: STYLE }
         HomePage {}
     }
